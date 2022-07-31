811c5701fe29baca1b15f47174a9d899599aca64
Rick Rieger <rickrieger36251@gmail.com>
2022-07-31 09:44:13 -0400

    db connected and display jokes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,23 +1,23 @@
-import type { LinksFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import type { LinksFunction } from '@remix-run/node';
+import { Link } from '@remix-run/react';
 
-import stylesUrl from "~/styles/index.css";
+import stylesUrl from '~/styles/index.css';
 
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: stylesUrl }];
+  return [{ rel: 'stylesheet', href: stylesUrl }];
 };
 
 export default function IndexRoute() {
   return (
-    <div className="container">
-      <div className="content">
+    <div className='container'>
+      <div className='content'>
         <h1>
           Remix <span>Jokes!</span>
         </h1>
         <nav>
           <ul>
             <li>
-              <Link to="jokes">Read Jokes</Link>
+              <Link to='jokes'>Read Jokes</Link>
             </li>
           </ul>
         </nav>
